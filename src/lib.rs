d81a39215ca67813ab97bb84dbcf820d79b9587c
lers梦魔 <lersnightmare@gmail.com>
2024-03-20 23:23:32 +0800

    FIX BUGS

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,49 @@
 use std::ptr::null_mut;
 
-use crate::dword::ServiceStatus;
 use widestring::U16CString;
-use winapi::um::winsvc::{SC_HANDLE, SERVICE_QUERY_STATUS, SERVICE_START, SERVICE_STOP};
 use winapi::{
     shared::minwindef::DWORD,
     um::{
         errhandlingapi::GetLastError,
         winsvc::{
-            ControlService, DeleteService, OpenSCManagerW, OpenServiceW, QueryServiceStatus,
-            StartServiceW, SC_MANAGER_CONNECT, SERVICE_CONTROL_STOP,
+            ControlService, OpenSCManagerW, OpenServiceW, QueryServiceStatus,
+            SC_MANAGER_CONNECT, SERVICE_CONTROL_STOP, StartServiceW,
         },
     },
 };
+use winapi::um::winsvc::{SC_HANDLE, SERVICE_QUERY_STATUS, SERVICE_START, SERVICE_STOP};
+
+use crate::dword::ServiceStatus;
 
 pub mod dword {
     use std::collections::HashMap;
     use std::fmt::{Display, Formatter};
+
+    use lazy_static::lazy_static;
     use winapi::shared::minwindef::DWORD;
 
     pub struct ServiceStatus {
         pub kind: DWORD,
     }
 
-    const STATUS: HashMap<DWORD, STATUS> = HashMap::from([
-        (DWORD::from(1u8), STATUS::SERVICE_STOPPED),
-        (DWORD::from(2u8), STATUS::SERVICE_START_PENDING),
-        (DWORD::from(3u8), STATUS::SERVICE_STOP_PENDING),
-        (DWORD::from(4u8), STATUS::SERVICE_RUNNING),
-        (DWORD::from(5u8), STATUS::SERVICE_CONTINUE_PENDING),
-        (DWORD::from(6u8), STATUS::SERVICE_PAUSE_PENDING),
-        (DWORD::from(7u8), STATUS::SERVICE_PAUSED),
-    ]);
+    lazy_static! {
+        static ref SERVICE_STATUS: HashMap<u8, STATUS> = {
+            let map: HashMap<u8, STATUS> = HashMap::from([
+                (1u8, STATUS::SERVICE_STOPPED),
+                (2u8, STATUS::SERVICE_START_PENDING),
+                (3u8, STATUS::SERVICE_STOP_PENDING),
+                (4u8, STATUS::SERVICE_RUNNING),
+                (5u8, STATUS::SERVICE_CONTINUE_PENDING),
+                (6u8, STATUS::SERVICE_PAUSE_PENDING),
+                (7u8, STATUS::SERVICE_PAUSED),
+            ]);
+            map
+        };
+    }
 
     #[derive(Debug)]
     #[allow(non_camel_case_types)]
+    #[derive(PartialEq)]
     pub enum STATUS {
         SERVICE_STOPPED,
         SERVICE_START_PENDING,
@@ -47,12 +56,13 @@ pub mod dword {
 
     impl Display for ServiceStatus {
         fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            if STATUS.contains_key(&self.kind) {
+            let num = self.kind as u8;
+            if SERVICE_STATUS.contains_key(&num) {
                 write!(
                     f,
                     "Service Status({}):{:?}",
                     &self.kind,
-                    STATUS.get(&self.kind)?
+                    SERVICE_STATUS.get(&num).unwrap()
                 )
             } else {
                 write!(f, "UNKNOWN STATUS:{:?}", &self.kind)
@@ -61,8 +71,8 @@ pub mod dword {
     }
 
     impl ServiceStatus {
-        pub fn eq(&self, other: STATUS) -> bool {
-            let value = STATUS.get(&self.kind)?;
+        pub fn eq(&self, other: &STATUS) -> bool {
+            let value = SERVICE_STATUS.get(&(self.kind as u8)).unwrap();
             return value == other;
         }
     }
@@ -70,7 +80,7 @@ pub mod dword {
 #[cfg(windows)]
 pub struct WindowsService {
     service_handle: SC_HANDLE,
-    service_name: &'static str,
+    pub service_name: &'static str,
 }
 
 #[cfg(windows)]
@@ -162,15 +172,15 @@ impl WindowsService {
         }
     }
 
-    /// 删除服务
-    fn delete_service(&self) -> Result<(), DWORD> {
-        let result = unsafe { DeleteService(self.service_handle) };
-        if result == 0 {
-            Err(unsafe { GetLastError() })
-        } else {
-            Ok(())
-        }
-    }
+    // /// 删除服务
+    // fn delete_service(&self) -> Result<(), DWORD> {
+    //     let result = unsafe { DeleteService(self.service_handle) };
+    //     if result == 0 {
+    //         Err(unsafe { GetLastError() })
+    //     } else {
+    //         Ok(())
+    //     }
+    // }
 }
 
 #[cfg(test)]
